24b99a9abf64a15f8e832fda363e33f87dcfac18
lucas chagas <lucazchagaz@outlook.com>
2022-08-25 08:23:43 -0300

    adição do hook e componente de paginação

---
+++ app/src/Components/Layout/Pagination/pagination.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {  usePagination, DOTS } from "../../../hook/usePagination"
+
+const Pagination = props => {
+  const {
+    onPageChange,
+    totalCount,
+    siblingCount = 1,
+    currentPage,
+    pageSize,
+    className
+  } = props;
+  
+  const paginationRange = usePagination({
+    currentPage,
+    totalCount,
+    siblingCount,
+    pageSize
+  });
+
+  if (currentPage === 0 || paginationRange.length < 2) {
+    return null;
+  }
+
+  const onNext = () => {
+    onPageChange(currentPage + 1);
+  };
+
+  const onPrevious = () => {
+    onPageChange(currentPage - 1);
+  };
+
+  let lastPage = paginationRange[paginationRange.length - 1];
+
+  return (
+    <ul
+      className={'pagination-container'}
+    >
+      <li
+        className={'pagination-item'}
+        onClick={onPrevious}
+      >
+        <div className="arrow left" />
+      </li>
+      {paginationRange.map(pageNumber => {
+         
+        if (pageNumber === DOTS) {
+          return <li className="pagination-item dots">&#8230;</li>;
+        }
+		
+        return (
+          <li
+            className={'pagination-item'}
+            onClick={() => onPageChange(pageNumber)}
+          >
+            {pageNumber}
+          </li>
+        );
+      })}
+
+      <li
+        className={'pagination-item'}
+        onClick={onNext}
+      >
+        <div className="arrow right" />
+      </li>
+    </ul>
+  );
+};
+
+export default Pagination;
