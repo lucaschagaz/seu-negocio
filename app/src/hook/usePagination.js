1a86e8e898ade7b388ae1252ebb4a596e8230de0
lucas chagas <lucazchagaz@outlook.com>
2022-08-29 15:06:26 -0300

    correção no bug na paginação

--- app/src/hook/usePagination.js
+++ app/src/hook/usePagination.js
@@ -2,14 +2,16 @@ import { useMemo } from "react";
 
 export const DOTS = "..."
 
+const range = (start, end) => {
+  let length = end - start + 1;
+  return Array.from({ length }, (_, idx) => idx + start);
+};
+
+
 export const usePagination = ({ totalCount, pageSize, siblingCount = 1, currentPage}) => {
    
     const paginationRange = useMemo(() => {
-        const range = (start, end) => {
-          let length = end - start + 1;
-          return Array.from({ length }, (_, idx) => idx + start);
-        };
-
+       
       const totalPageCount = Math.ceil(totalCount / pageSize);
   
       const totalPageNumbers = siblingCount + 5;
